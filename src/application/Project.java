29a01b004cc9f2fb03bdf7b02e7bf65ac569359c
imPepoDev <pedrogabrielofc@hotmail.com>
2025-03-09 11:38:29 -0300

    Special move - Promotion

--- src/application/Project.java
+++ src/application/Project.java
@@ -41,7 +41,11 @@ public class Project {
 				if (chessMatch.getPromoted() != null) {
 					System.out.print("Enter piece for promotion (B/N/R/Q): ");
 					String type = sc.nextLine().toUpperCase();
-					chessMatch.replacePromotedPiece(type);
+ 					while (!type.equals("B") && !type.equals("N") && !type.equals("R") & !type.equals("Q")) {
+ 						System.out.print("Invalid value! Enter piece for promotion (B/N/R/Q): ");
+ 						type = sc.nextLine().toUpperCase();
+ 					}
+ 					chessMatch.replacePromotedPiece(type);
 				}
 			}
 			catch (ChessException e) {
